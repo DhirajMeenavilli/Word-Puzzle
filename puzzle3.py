ea64c574cdf1e4ad7ffa34fbf2fc0e0b6ab0098a
Dhiraj <d.meenavilli@share.epsb.ca>
2019-10-01 05:32:31 -0600

    Version 3 fully operational, now to go do version 1 and 2 as instructed

--- puzzle3.py
+++ puzzle3.py
@@ -3,30 +3,70 @@ Author: Dhiraj Meenavilli
 Date: October/01/2019
 Title: Word Puzzle Version 3
 """
-
+import random
 ############ --------------- Variables --------------------- ##############
-
-
-
-
+filename = 'instructions.txt'
+is_win = False
+num_guesses = 4
+words = ['kiwi','bannana','feet','nine']
+answear = words[random.randrange(1)]
+puzzle = []
 ########### --------------- Functions ---------------------- ###############
 def display_instructions(filename):
-	
-def display_puzzle_string(puzzle):
+	instructions = open(filename,"r")
+	instructions = instructions.read()
+	print(instructions)
 
-def play_game(puzzle, answear):
+def display_puzzle_string(puzzle):
+	print(puzzle)
 
 def get_guess(num_guesses):
+	guess = input("What do you think the letters in this word are ")
+	print("Guesses:",num_guesses)
+	return guess
+
 
 def update_puzzle_string(puzzle, answear, guess):
+	update = 0
+	for i in range(len(answear)):
+		if guess == answear[i]:
+			if puzzle[i] == guess:
+				update = 0
+			else:
+				puzzle[i] = guess
+				update = 1
+	return update
 
 def is_word_found(puzzle):
+	complete = 1
+	for i in range(len(puzzle)):
+		if puzzle[i] == "_":
+			complete = 0
+	return complete
 
-def display_results(is_win,answear)
+def display_results(is_win,answear):
+	if is_win == True:
+		print("Congratulations you succesfully guessed", answear)
 
-def play_game():
-	get_guess()
-	update_puzzle_string()
-	is_word_found()
+	if is_win == False:
+		print("Unfortunatley you've run out of guesses the answear was", answear)
+
+def play_game(puzzle,answear,num_guesses):
+	guess = get_guess(num_guesses)
+	update = update_puzzle_string(puzzle,answear,guess)
+	complete = is_word_found(puzzle)
+	if complete == 1:
+		return True
+	else:
+		return False
 ########### --------------- Program Starts Here ------------ ###############
-play_game()
+for i in range(len(answear)):
+	puzzle.append("_")
+
+display_instructions(filename)
+while num_guesses > 0 and is_win == False:
+	is_win = play_game(puzzle,answear,num_guesses)
+	display_puzzle_string(puzzle)
+	num_guesses = num_guesses - 1
+
+display_results(is_win,answear)
